e5375f00e467f5ffca4caf0b6a692d36b6db1efd
Jonghwa <whdghk3471@gmail.com>
2026-02-24 12:44:26 +0900

    #5.3 Realm SDK

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,44 @@
 import Navigator from "@/components/navigator";
+import { SplashScreen } from "expo-router";
+import { useEffect, useState } from "react";
+import Realm from "realm";
+
+SplashScreen.preventAutoHideAsync();
+
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const realm = await Realm.open({
+          path: "nomadDiaryDB",
+          schema: [FeelingSchema],
+        });
+        console.log(realm);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (ready) {
+      SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
   return <Navigator />;
 }
